572441f71b4578bbc0a621ce2066561767c3737e
JoeLee9981 <joelee19981@gmail.com>
2014-05-06 22:07:34 -0600

    added files to git

---
+++ LootRoller.rb
@@ -0,0 +1,169 @@
+require_relative 'LootTableManager'
+
+class LootRoller
+  #constant variables for index purposes
+  LEVEL = 0
+  TYPE = 1
+  LOW = 2
+  HIGH = 3
+  DIE = 4
+  SCALE = 5
+  COINS = 6
+  SUB = 7
+  
+  def do_roll(die = 6)
+    if die == 0
+      return 0
+    end
+    return 1 + Random.rand(die)  
+  end
+  
+  def do_loot_roll(levels)
+    #Tally coins for all rolls
+    total_coin = Hash["platinum", 0, "gold", 0, "silver", 0, "copper", 0]
+    all_items = Array.new
+    all_goods = Array.new
+    levels.each do |level|
+      roll = do_roll(100)
+      coins = get_coin_roll(level, roll)
+      coin = roll_coins(coins[DIE], coins[SCALE])
+      total_coin[coins[COINS]] += coin
+      display_roll(coins[LEVEL], coins[TYPE], roll, coins[LOW], coins[HIGH], coins[DIE], coins[SCALE], coins[COINS], coins[SUB], coin)
+      
+      roll = do_roll(100)
+      goods = get_goods_roll(level, roll)
+      goods_list = roll_others(goods[DIE], goods[SUB])
+      display_roll(goods[LEVEL], goods[TYPE], roll, goods[LOW], goods[HIGH], goods[DIE], goods[SCALE], goods[COINS], goods[SUB], goods_list)
+      all_goods.push(*goods_list)
+      
+      roll = do_roll(100)
+      items = get_items_roll(level, roll)
+      items_list = roll_others(items[DIE], items[SUB])
+      display_roll(items[LEVEL], items[TYPE], roll, items[LOW], items[HIGH], items[DIE], items[SCALE], items[COINS], items[SUB], items_list)
+      all_items.push(*items_list)
+    end
+    puts "Total Coin: "
+    total_coin.each do |val|
+      split_coin = val[1] / 8
+      puts "  #{val[1]} #{val[0]} coins\t-\t#{split_coin} each"
+    end
+    puts "Goods:"
+    all_goods.each do |good|
+      puts "  #{good}"
+    end
+    puts "Items:"
+    all_items.each do |item|
+      puts "  #{item}"
+    end
+  end
+  
+  private
+  def roll_coins(die, scale)
+    coins = 0
+    dice = die.split('d')
+    if dice[1] == 0
+      return 0
+    end
+    Integer(dice[0]).times do |i|
+      result = do_roll(Integer(dice[1]))
+      coins += result
+    end
+    return coins * Integer(scale)
+  end
+  
+  def roll_others(die, sub)
+    goods = Array.new
+    dice = die.split('d')
+    if dice[1] == '0'
+      return goods
+    end
+    sum = 0
+    Integer(dice[0]).times do |i|
+      result = do_roll(Integer(dice[1]))
+      sum += result
+    end
+    sum.times do |i|
+      r = do_roll(100)
+      if sub.chomp == "gems" || sub.chomp == "art"
+        goods.push(get_goods(sub, r))
+      else
+        goods.push("#{r} #{sub.chomp}")
+      end
+    end
+    return goods
+  end
+  
+  def get_coin_roll(level, roll)
+    manager = DbManager.new
+    loot_roll = manager.find_from_db(level, "coins", roll)
+    return loot_roll
+  end
+  
+  def get_goods_roll(level, roll)
+    manager = DbManager.new
+    loot_roll = manager.find_from_db(level, "goods", roll)
+    return loot_roll
+  end
+  
+  def get_items_roll(level, roll)
+    manager = DbManager.new
+    loot_roll = manager.find_from_db(level, "items", roll)
+    return loot_roll
+  end
+  
+  def get_goods(type, roll)
+    manager = DbManager.new
+    list = manager.find_goods_from_db(type, roll)
+    return list[Random.rand(list.length)]
+  end
+  
+  def display_roll(level, type, roll, low, hi, die, scale, coin_type, subtype, total)
+    puts "======================================================"
+    puts "Level: #{level}\ttype: #{type}\t#{roll}"
+    if type == "coins"
+      puts "  #{low} - #{hi}\t#{die} * #{scale}\t#{coin_type}"
+      if total == 0
+        puts "  ***RESULTS: No Coin***"
+      else
+        puts "  ***RESULTS: #{total} #{coin_type} coins***"
+      end
+    else
+      puts "  #{low} - #{hi}\t#{die}\t#{subtype}"
+      if total.size == 0
+        puts " ***RESULTS: No Goods"
+      else
+        result_str = "  ***RESULTS: "
+        total.each do |item|
+          result_str += item.to_s + ", "
+        end
+        puts result_str
+      end
+    end
+    puts
+  end
+end
+
+def build_list(input)
+  splits = input.split(',')
+  chal_rates = Array.new
+  splits.each do |val|
+    chal_rates.push(Integer(val))
+  end
+end
+
+def start
+  roller = LootRoller.new
+  while true
+    puts "Please entere a challenge rating (or 'x' to exit):"
+    input = gets.chomp
+    if(input == 'x' || input == 'X')
+      exit
+    end
+    chal_rates = build_list(input)
+    roller.do_loot_roll(chal_rates)
+  end
+end
+
+if __FILE__ == $0
+  start
+end
---
+++ LootTableManager.rb
@@ -0,0 +1,116 @@
+require 'sqlite3'
+
+class DbManager
+ 
+  def import_from_csv(filename)
+    file = File.new(filename, "r")
+    file.gets
+    while (line = file.gets)
+      a = line.split(',')
+      #puts "#{a[0]}, #{a[1]}, #{a[2]}, #{a[3]}, #{a[4]}, #{a[5]}, #{a[6]}, #{a[7]}"
+      insert_to_db(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7])
+    end
+    
+    file.close
+  end
+  
+  def import_art_from_csv(filename)
+    file = File.new(filename, "r")
+    file.gets
+    while (line = file.gets)
+      a = line.split(',')
+      insert_to_art_db(a[0], a[1], a[2], a[3], a[4])
+    end
+  end
+  
+  def import_gems_from_csv(filename)
+    file = File.new(filename, "r")
+    file.gets
+    while (line = file.gets)
+      a = line.split(',')
+      insert_to_gems_db(a[0], a[1], a[2], a[3], a[4])
+    end
+  end
+  
+  def insert_to_art_db(name, low, high, value, avg_val)
+    begin
+      db = SQLite3::Database.open "DNDTABLES.sqlite3"
+      cmd = "INSERT INTO art (name,low,high,value,avg_val) VALUES (?,?,?,?,?)"
+      db.execute(cmd, name, low, high, value, avg_val)
+    rescue SQLite3::Exception => e
+      puts "Exception #{e}"
+    ensure
+      db.close if db
+    end
+  end
+  
+  def insert_to_gems_db(name, low, high, value, avg_val)
+    begin
+      db = SQLite3::Database.open "DNDTABLES.sqlite3"
+      cmd = "INSERT INTO gems (name,low,high,value,avg_val) VALUES (?,?,?,?,?)"
+      db.execute(cmd, name, low, high, value, avg_val)
+    rescue SQLite3::Exception => e
+      puts "Exception #{e}"
+    ensure
+      db.close if db
+    end
+  end
+  
+  def insert_to_db(level, type, low, high, die, scale, coin, sub)
+    begin
+      db = SQLite3::Database.open "DNDTABLES.sqlite3"
+      cmd = "INSERT INTO loot_rolls (level,type,low_roll,high_roll,die,scale,coin_type,sub_type) VALUES (?,?,?,?,?,?,?,?)"
+      db.execute(cmd, level,type,low,high,die,scale,coin,sub)
+    rescue SQLite3::Exception => e
+      puts "Exception: #{e}"
+    ensure
+      db.close if db
+    end
+  end
+  
+  def find_from_db(level, type, roll)
+    begin
+      db = SQLite3::Database.open("DNDTABLES.sqlite3")
+      cmd = db.prepare("SELECT * FROM loot_rolls WHERE level = ? and type = ? and (? >= low_roll and ? <= high_roll)")
+      cmd.bind_params(level, type, roll, roll)
+      result = cmd.execute
+      a = result.next
+      return a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[0]
+    rescue SQLite3::Exception => e
+      puts "Exception: #{e}"
+    ensure
+      cmd.close if cmd
+      db.close if db
+    end
+  end
+  
+  def find_goods_from_db(table, roll)
+    begin
+      db = SQLite3::Database.open("DNDTABLES.sqlite3")
+      cmd = db.prepare("SELECT name FROM #{table} WHERE ? >= low and ? <= high")
+      cmd.bind_params(roll, roll)
+      result = cmd.execute
+      a = Array.new
+      result.each do |name|
+        a.push(name[0].chomp)
+      end
+      return a
+    rescue SQLite3::Exception => e
+      puts "Exception: #{e}"
+    ensure
+      cmd.close if cmd
+      db.close if db
+    end
+  end
+  
+end
+
+def start
+  manager = DbManager.new
+  manager.find_goods_from_db("art", 55)
+  manager.find_goods_from_db("gems", 25)
+end
+
+if __FILE__ == $0
+  start
+end
